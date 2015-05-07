0080ce09b8d74a325ff04f816a488cf5073d607e
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2015-05-06 21:08:56 -0300

    mudando para não usa amostras, e colocando para remover duplicados (tinha 101 projs que tava, duplicados!!!)

--- relatorio.R
+++ relatorio.R
@@ -5,29 +5,18 @@ source('utils.R')
 gh <- read.table("repos_cmt_day_maior_age.csv",
   header=TRUE, sep=";", na.strings="NA", dec=".", strip.white=TRUE)
 
+#remove dupls!
+gh <- gh[!duplicated(gh$Name),]
+
 attach(gh)
 gh_users <-  gh[which(Owner.Type=='User'),]
 gh_org <-  gh[which(Owner.Type=='Organization'),]
 detach(gh)
 
-# 134 organizacoes
-# 167 users
-
-attach(gh_users)
-gh_users_sample <- gh_users[sample(1:nrow(gh_users), 167, replace=FALSE),]
-detach(gh_users)
-
-attach(gh_org)
-gh_org_sample <- gh_org[sample(1:nrow(gh_org), 134, replace=FALSE),]
-detach(gh_org)
-
-gh_sample_final <- mergeRows(gh_users_sample, gh_org_sample, common.only=FALSE)
-
 ## hist e plots
 dfplot(gh, 'total')
-dfplot(gh_users_sample, 'user')
-dfplot(gh_org_sample, 'org')
-dfplot(gh_sample_final, 'final')
+dfplot(gh_users, 'user')
+dfplot(gh_org, 'org')
 
 ### shapiro test
 
@@ -40,31 +29,23 @@ sink()
 # user
 filename <- paste("shapiro_test/", 'user', '_', 'all.txt', sep='')
 sink(filename, append=FALSE, split=FALSE)
-    allShapiro(gh_users_sample)
+    allShapiro(gh_users)
 sink()
 
 # Organization
 filename <- paste("shapiro_test/", 'org', '_', 'all.txt', sep='')
 sink(filename, append=FALSE, split=FALSE)
-    allShapiro(gh_org_sample)
-sink()
-
-# final
-filename <- paste("shapiro_test/", 'final', '_', 'all.txt', sep='')
-sink(filename, append=FALSE, split=FALSE)
-    allShapiro(gh_sample_final)
+    allShapiro(gh_org)
 sink()
 
 # Infos sobre Porcentagens das Qualitativas:
 porcentagens(gh, 'total')
-porcentagens(gh_users_sample, 'user')
-porcentagens(gh_org_sample, 'org')
-porcentagens(gh_sample_final, 'final')
+porcentagens(gh_users, 'user')
+porcentagens(gh_org, 'org')
 
 # Correlacoes
 correlacoes(gh, 'total')
-correlacoes(gh_users_sample, 'user')
-correlacoes(gh_org_sample, 'org')
-correlacoes(gh_sample_final, 'final')
+correlacoes(gh_users, 'user')
+correlacoes(gh_org, 'org')
 
 
